27db415d5b3a941255ea28f3a3b149813ba1f6ec
LoLLiPoP1921 <jer.bog@hotmail.com>
2025-07-16 12:51:22 +0200

    established the game window

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    
+    pygame.init
+    print("'Starting Asteroids!'")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    gameExit = False
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
 
+        screen.fill(SCREEN_COLOR)
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
