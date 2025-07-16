14092922745559597e453d53c276a0f8dac0de5b
LoLLiPoP1921 <jer.bog@hotmail.com>
2025-07-16 13:17:28 +0200

    CircleShape class added

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     gameExit = False
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     
     while not gameExit:
         for event in pygame.event.get():
@@ -16,7 +19,12 @@ def main():
                 gameExit = True
 
         screen.fill(SCREEN_COLOR)
-        pygame.display.update()
+        
+        
+        
+        
+        pygame.display.flip()
+        dt = clock.tick(60)
 
 if __name__ == "__main__":
     main()
