17ce8db914442f88c5ff3001408362d156f5fd8c
LoLLiPoP1921 <jer.bog@hotmail.com>
2025-07-16 16:38:59 +0200

    created astroide player movement

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     
-    pygame.init
+    pygame.init()
     print("'Starting Asteroids!'")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +15,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()    
     
+
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,11 +33,14 @@ def main():
 
         screen.fill(SCREEN_COLOR)
         
+        for sprite in drawable:
+            sprite.draw(screen)
         
+        updatable.update(dt)
         
         
         pygame.display.flip()
-        dt = clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
